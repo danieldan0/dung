2f2fc8457a488692da6bc6563811708d073c0ba4
Daniel Nakushnov <daniel.nakushnov@gmail.com>
2017-05-28 23:12:13 +0300

    Create demo
    Just a working demo using webpack.

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+
+module: {
+  rules: [
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['env']
+        }
+      }
+    }
+  ]
+}
+
+module.exports = {
+  entry: './app/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
