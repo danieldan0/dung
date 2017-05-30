a4b3ff954bfc1dd811293eaed91c21a3a24eb9a0
Ian Bro <ianbrode@gmail.com>
2017-05-30 17:56:53 +0300

    Fix malfored config

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,29 @@
 var path = require('path');
 
-module: {
-  rules: [
-    {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['env']
-        }
-      }
-    }
-  ]
-}
-
 module.exports = {
   entry: './app/game.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    root: [
+      path.resolve('./app'),
+      path.resolve('./node_modules')
+    ]
   }
 };
-
-resolve: {
-  root: [
-    path.resolve('./app'),
-    path.resolve('./node_modules')
-  ]
-},
