6f30b592da6e7cba8a9ac2b1715775341c5b67ab
Daniel Nakushnov <daniel.nakushnov@gmail.com>
2017-06-04 01:56:20 +0300

    Add rot.js support check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+import ROT from 'rot-js'
 import game from './game';
 
-game.init();
+window.onload = function() {
+    // Check if rot.js can work on this browser
+    if (!ROT.isSupported()) {
+        alert("The rot.js library isn't supported by your browser.");
+    } else {
+        // Initialize the game
+        game.init();
+    }
+}
