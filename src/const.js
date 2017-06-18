5cd6c1a6c18f732c250cb546203538bfa7e5c68d
Daniel Nakushnov <daniel.nakushnov@gmail.com>
2017-06-19 00:39:28 +0300

    Refactored code

--- src/const.js
+++ src/const.js
@@ -1,9 +1,23 @@
-export class DisplayOptions {
-    static width = 80
-    static height = 25
+const DisplayOptions {
+    width: 80,
+    height: 25
 }
 
-export class MapOptions {
-    static width = 80
-    static height = 25
+const MapOptions {
+    width: 80,
+    height: 25
 }
+
+const Tiles = {
+    "null": {
+        glyph: new Glyph()
+    },
+    floor: {
+        glyph: new Glyph(".", "#444", "#222")
+    },
+    wall: {
+        glyph: new Glyph("#", "#777", "#2e2e2e")
+    }
+}
+
+export {DisplayOptions, MapOptions, Tiles};
--- src/tile.js
+++ src/tile.js
@@ -1,19 +1,9 @@
 import Glyph from './glyph'
+import Tiles drom './tile'
 
 export default class Tile {
     constructor(type) {
-        const tile = Tile.Types[type];
+        const tile = Tiles[type];
         this.glyph = tile.glyph;
-    }
-    static Types = {
-        null: {
-            glyph: new Glyph()
-        },
-        floor: {
-            glyph: new Glyph(".", "#444", "#222")
-        },
-        wall: {
-            glyph: new Glyph("#", "#777", "#2e2e2e")
-        }
     }
 }
